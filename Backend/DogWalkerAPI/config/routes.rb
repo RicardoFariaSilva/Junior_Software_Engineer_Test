ce9e2b1850befc2c822aaf46a0f4a8825e6f6f1f
ricardo.faria.silva <ricardo.faria.silva@usp.br>
2019-09-08 23:43:49 -0300

    [backend] Configuring routes

--- Backend/DogWalkerAPI/config/routes.rb
+++ Backend/DogWalkerAPI/config/routes.rb
@@ -1,4 +1,25 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # clients
+  namespace :api do
+    namespace :v1 do
+      resources :clients
+    end
+  end
+
+  # dogs
+  namespace :api do
+    namespace :v1 do
+      resources :dogs
+    end
+  end
+
+  # walkers
+  namespace :api do
+    namespace :v1 do
+      resources :walkers
+    end
+  end
 end
