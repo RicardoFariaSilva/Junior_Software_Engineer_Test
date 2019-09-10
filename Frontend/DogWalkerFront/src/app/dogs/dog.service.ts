44f1cfb773921b13f4830dbc53a479ace033e97e
ricardo.faria.silva <ricardo.faria.silva@usp.br>
2019-09-10 01:31:20 -0300

    [frontend] Adding dog service

---
+++ Frontend/DogWalkerFront/src/app/dogs/dog.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { Dog } from './dog';
+import { Observable } from 'rxjs';
+
+const url = 'https://floating-lowlands-65453.herokuapp.com/api/v1/dogs';
+const email = 'online@teste.com';
+const token = 'YeuHBHsJyDsGVFoUD9Ep';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class DogService {
+
+  constructor(private http: HttpClient) { }
+
+  getDogs(): Observable<Dog[]> {
+    return this.http.get<Dog[]>(url, {headers: {'X-User-Email': email, 'X-User-Token': token}});
+  }
+
+  getDogById(id: number): Observable<Dog> {
+    const newUrl = url + '/'  + id;
+    return this.http.get<Dog>(newUrl, {headers: {'X-User-Email': email, 'X-User-Token': token}});
+  }
+}
