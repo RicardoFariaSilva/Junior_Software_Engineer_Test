2707d6a8a085448b8b7af54d0b8fdda4bb88bd77
ricardo.faria.silva <ricardo.faria.silva@usp.br>
2019-09-10 01:30:56 -0300

    [frontend] Adding client service

---
+++ Frontend/DogWalkerFront/src/app/clients/client.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Client } from './client';
+
+const url = 'https://floating-lowlands-65453.herokuapp.com/api/v1/clients';
+const email = 'online@teste.com';
+const token = 'YeuHBHsJyDsGVFoUD9Ep';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ClientService {
+
+  constructor(private http: HttpClient) { }
+
+  getClients(): Observable<Client[]> {
+    return this.http.get<Client[]>(url, {headers: {'X-User-Email': email, 'X-User-Token': token}});
+  }
+
+  getClientById(id: number): Observable<Client> {
+    const newUrl = url + '/'  + id;
+    return this.http.get<Client>(newUrl, {headers: {'X-User-Email': email, 'X-User-Token': token}});
+  }
+
+}
