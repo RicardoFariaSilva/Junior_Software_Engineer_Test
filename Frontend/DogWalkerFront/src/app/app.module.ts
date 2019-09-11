f96ec72d329638cd670f5f059570ceb6254a0495
ricardo.faria.silva <ricardo.faria.silva@usp.br>
2019-09-11 00:04:48 -0300

    [frontend] Adding forms module import

--- Frontend/DogWalkerFront/src/app/app.module.ts
+++ Frontend/DogWalkerFront/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -29,6 +30,7 @@ import { ShowDogComponent } from './dogs/show-dog/show-dog.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     HttpClientModule
   ],
   providers: [],
